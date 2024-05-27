2e9f964be6c7871b897cc6d686ebb435ff3bcf19
Sanae810 <Chara82510@gmail.com>
2024-05-27 09:44:00 +0800

    05.27.9.43

--- project/app/src/main/java/com/example/aircraftwar/MainActivity.java
+++ project/app/src/main/java/com/example/aircraftwar/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.aircraftwar;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private boolean isSoundEffectOn = false;
+    Button btnOffline;
 
-public class MainActivity extends AppCompatActivity {
+    RadioGroup rg;
+    RadioButton rbOn;
+    RadioButton rbOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView(){
+        btnOffline = (Button) findViewById(R.id.btnOffline);
+        rg = (RadioGroup) findViewById(R.id.rg);
+        rbOff = (RadioButton) findViewById(R.id.rbOff);
+        rbOn = (RadioButton) findViewById(R.id.rbOn);
+
+        btnOffline.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view){
+        int selectedId = rg.getCheckedRadioButtonId();
+        if (selectedId == R.id.rbOn){
+            isSoundEffectOn = true;
+        }
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("Boolean",isSoundEffectOn);
+        Intent intent = new Intent(MainActivity.this, OfflineActivity.class);
+        intent.putExtra("sound", bundle);
+        startActivity(intent);
     }
 }
--- project/app/src/main/java/com/example/aircraftwar/OfflineActivity.java
+++ project/app/src/main/java/com/example/aircraftwar/OfflineActivity.java
@@ -1,16 +1,43 @@
 package com.example.aircraftwar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class OfflineActivity extends AppCompatActivity {
-
+    private Boolean isSoundEffectOn;
+    TextView tvTitle;
+    Button btnEasy;
+    Button btnNormal;
+    Button btnHard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_offline);
+
+        Intent receiveIntent = getIntent();
+        Bundle bundle = receiveIntent.getBundleExtra("sound");
+        isSoundEffectOn = (Boolean) bundle.get("Boolean");
+        String test = "00000000000000000";
+
+        if(isSoundEffectOn == true) test = "111111111111111";
+        initView();
+
+        Toast.makeText(this, test, Toast.LENGTH_LONG).show();
+
+
+    }
+
+    private void initView(){
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        btnEasy = (Button) findViewById(R.id.btnEasy);
+        btnNormal = (Button) findViewById(R.id.btnNormal);
+        btnHard = (Button) findViewById(R.id.btnHard);
     }
 
 
