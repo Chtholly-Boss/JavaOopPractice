a78bff4c9d2a8dc4ad4238ff21d2fc8e3a231a8b
Cauchy2b <841907997@qq.com>
2024-05-27 09:49:48 +0800

    Myb repo gradle update

--- project/app/src/main/java/com/example/aircraftwar/OfflineActivity.java
+++ project/app/src/main/java/com/example/aircraftwar/OfflineActivity.java
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class OfflineActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
